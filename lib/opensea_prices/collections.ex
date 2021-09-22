474e1066bd5c07b8873e98a35cc5f27154429711
Joshua Plicque <plicjo@gmail.com>
2021-09-22 11:48:15 -0400

    Add connections to collections API endpoint

--- lib/opensea_prices/collections.ex
+++ lib/opensea_prices/collections.ex
@@ -1,20 +1,20 @@
 defmodule OpenseaPrices.Collections do
+  alias OpenseaPrices.OpenseaApiClient
+  @limit 300
+  @initial_offset 0
 
-  alias OpenseaPrices.Apiclient
-
-  def get_collections_info(), do: get_collections(get_assets())
-
-  def get_assets() do
-    {:ok, %Tesla.Env{body: %{"assets" => assets}}} = Apiclient.get_request_assets()
-    assets
+  def list_collections() do
+    results = OpenseaApiClient.get_collections([limit: @limit, offset: @initial_offset])
+    list_collections(results, @initial_offset + 300, Enum.count(results))
   end
 
-  def get_collections(assets) do
-    Enum.map(assets, fn %{"asset_contract" => asset_contract, "token_id" => token_id} ->
-      {:ok, %Tesla.Env{body: %{"collection" => collection}}} = Apiclient.get_request_asset(%{token_id: token_id, contract_address: asset_contract["address"]})
-      Map.put(Map.new(), :name, collection["name"])
-      |> Map.put(:floor_price, collection["stats"]["floor_price"])
-      |> Map.put(:one_day_volume, collection["stats"]["seven_day_volume"])
-    end)
+  def list_collections(list, offset, last_result_count = 300) do
+    results = OpenseaApiClient.get_collections([limit: @limit, offset: offset])
+    list_collections(list ++ results, offset + 300, Enum.count(results))
+  end
+
+  # API limits results at 50_000 offset
+  def list_collections(list, 47_700, last_result_count) do
+    list
   end
 end
--- lib/opensea_prices/opensea_api_client.ex
+++ lib/opensea_prices/opensea_api_client.ex
@@ -5,6 +5,12 @@ defmodule OpenseaPrices.OpenseaApiClient do
 
   def get_collections(query_params \\ [limit: 300, offset: 0]) do
     {:ok, %Tesla.Env{body: %{"collections" => collections}}} = get("https://api.opensea.io/api/v1/collections", query: query_params)
-    collections
+    Enum.map(collections, fn collection -> collection["name"] end)
+  end
+
+  # Sale price returns a 500
+  def get_assets(query_params \\ [limit: 10, order_by: "sale_count", order_direction: "desc"]) do
+    {:ok, %Tesla.Env{body: %{"assets" => assets}}} = get("https://api.opensea.io/api/v1/assets", query: query_params)
+    Enum.map(assets, fn asset -> asset["name"] end)
   end
 end
